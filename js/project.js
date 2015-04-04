247fc789257a456e7cad7e8f0859765012bb655e
RosaChoi <rosa.uniq@gmail.com>
2015-04-04 12:22:57 -0700

    close icon added to project menu and media query for small screen display

--- js/project.js
+++ js/project.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
   $('.projects').on('click', displayProjectMenu);
-
+  $('ul.navbar').on('click', 'span.fa-times', closeProject);
 });
 
 
 function displayProjectMenu() {
   var projects = $('#projects');
   var firstProject = $('#project1');
+  var projectCloseIcon = $('.projects .fa-times');
   var firstProjectCir = $('.proj-cir').find("[data='project1']");
 
   projects.fadeIn(2000);
+  projectCloseIcon.removeClass('hide').fadeIn(3000);
   firstProjectCir.attr('class', 'project-cir');
 
   firstProject.removeClass('hide').fadeIn(2000);
@@ -26,8 +28,18 @@ function displayProjects() {
     $('.project').addClass('hide');
     $('.project-cir').attr('class','hide');
     projectIcon.attr('class', 'project-cir');
-    projectContent.removeClass('hide').fadeIn(2000);
+    // projectContent.removeClass('hide').fadeIn(2000);
+    projectContent.removeClass('hide', function(){
+      fadeIn(3000)
+    });
 
   });
 
+}
+
+function closeProject() {
+    $('.project.container').fadeOut('2000', function(){
+      $('#projects').fadeOut('slow');
+      $('span.fa-times').addClass('hide');
+    });
 }
